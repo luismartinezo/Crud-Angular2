f1ff4f051f84af5f28433787c2931b8f1d35a95a
Luis Martinez <lmartinezocoro@gmail.com>
2019-06-12 00:08:29 -0500

    Primer commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Empleado } from './models/empleado';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,25 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'angular-simple-crud';
+  title = 'CRUD';
+  empleadoArray: Empleado[] = [
+    {id: 1, name: 'Luis', country: 'COLOMBIA'},
+    {id: 2, name: 'Carlos', country: 'MEXICO'},
+    {id: 3, name: 'Ryan', country: 'USA'}
+  ];
+
+  // Esto instancia de nuevo el modelo cada que se inicie lo coloca en blanco
+selectEmpleado: Empleado = new Empleado();
+
+addOrEdit(){
+
+  // Agregamos un dato mas al arreglo
+  this.selectEmpleado.id = this.empleadoArray.length + 1;
+  // Aca el empleado que esta llenando se guarda dentro del arreglo
+  this.empleadoArray.push(this.selectEmpleado);
+
+  // Para borrar los input luego de insertar
+  this.selectEmpleado = new Empleado();
+}
+
 }
