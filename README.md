43655b38d42a491a8cfc7d905f55d9a3f550c43a
Luis Martinez <lmartinezocoro@gmail.com>
2020-05-07 17:04:42 -0500

    Archivo Readme

--- README.md
+++ README.md
@@ -1,4 +1,8 @@
 # Angular Simple Crud
 
 Simple CRUD hecho en [Angular CLI](https://github.com/angular/angular-cli) version 7.3.1.
+Aplicacion simplemento realiza un push para visualizar en la mismas intefaz del usuario
 
+## intefaz
+
+![](../../../../assets/img/display.png?raw=true)
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,31 +1,29 @@
-import { Component } from '@angular/core';
-import { Empleado } from './models/empleado';
+import { Component } from "@angular/core";
+import { Empleado } from "./models/empleado";
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  selector: "app-root",
+  templateUrl: "./app.component.html",
+  styleUrls: ["./app.component.css"],
 })
 export class AppComponent {
-  title = 'CRUD';
+  title = "SIMPLY CRUD";
   empleadoArray: Empleado[] = [
-    {id: 1, name: 'Luis', country: 'COLOMBIA'},
-    {id: 2, name: 'Carlos', country: 'MEXICO'},
-    {id: 3, name: 'Ryan', country: 'USA'}
+    { id: 1, name: "Luis", country: "COLOMBIA" },
+    { id: 2, name: "Carlos", country: "MEXICO" },
+    { id: 3, name: "Ryan", country: "USA" },
   ];
 
   // Esto instancia de nuevo el modelo cada que se inicie lo coloca en blanco
-selectEmpleado: Empleado = new Empleado();
+  selectEmpleado: Empleado = new Empleado();
 
-addOrEdit(){
+  addOrEdit() {
+    // Agregamos un dato mas al arreglo
+    this.selectEmpleado.id = this.empleadoArray.length + 1;
+    // Aca el empleado que esta llenando se guarda dentro del arreglo
+    this.empleadoArray.push(this.selectEmpleado);
 
-  // Agregamos un dato mas al arreglo
-  this.selectEmpleado.id = this.empleadoArray.length + 1;
-  // Aca el empleado que esta llenando se guarda dentro del arreglo
-  this.empleadoArray.push(this.selectEmpleado);
-
-  // Para borrar los input luego de insertar
-  this.selectEmpleado = new Empleado();
-}
-
+    // Para borrar los input luego de insertar
+    this.selectEmpleado = new Empleado();
+  }
 }
